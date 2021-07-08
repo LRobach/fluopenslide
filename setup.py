876a466f0a0c58a07d395f3c9f7b218a0b29dcce
Louison Robach <louison.robach@gmail.com>
2021-07-08 11:24:34 +0200

    Bugs in the PyPI package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="fluopenslide",
-    version="0.0.2",
+    version="0.0.3",
     description="Opening fluorescence .czi files",
     author="Louison Robach",
     author_email="louison.robach@gmail.com",
