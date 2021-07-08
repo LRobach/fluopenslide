7b3c3f548524a8c901cd001f7a92ffbb8370f4c7
Louison Robach <louison.robach@gmail.com>
2021-07-08 11:15:48 +0200

    Changes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="fluopenslide",
-    version="0.0.1",
+    version="0.0.2",
     description="Opening fluorescence .czi files",
     author="Louison Robach",
     author_email="louison.robach@gmail.com",
@@ -20,7 +20,6 @@ setup(
         "aicspylibczi",
         "matplotlib",
         "pathlib",
-        "xml",
         "aicsimageio"
     ],
     include_package_data=True,
