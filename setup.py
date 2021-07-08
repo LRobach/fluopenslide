bb139d1abef487dfb836e53c2a7bab71f5398f62
Louison Robach <louison.robach@gmail.com>
2021-07-08 12:17:19 +0200

    New release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="fluopenslide1",
-    version="0.0.0",
+    version="0.0.1",
     description="Opening fluorescence .czi files",
     author="Louison Robach",
     author_email="louison.robach@gmail.com",
